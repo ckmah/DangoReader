737765d3cd5a9a4a092cd16fc456136794d1eaed
ckmah <clarence.iyan@gmail.com>
2015-07-08 19:09:17 -0700

    merge all

--- app/src/main/java/com/william/mangoreader/activity/BrowseMangaFragment.java
+++ app/src/main/java/com/william/mangoreader/activity/BrowseMangaFragment.java
@@ -38,10 +38,6 @@ public class BrowseMangaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
