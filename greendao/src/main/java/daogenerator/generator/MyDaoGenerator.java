76f21ee1a4c744d73bb487425610e12e477b7b78
William Xu <xu.will94@gmail.com>
2015-08-27 00:46:26 -0700

    changed database schema

--- greendao/src/main/java/daogenerator/generator/MyDaoGenerator.java
+++ greendao/src/main/java/daogenerator/generator/MyDaoGenerator.java
@@ -10,13 +10,19 @@ public class MyDaoGenerator {
         String src = "app/src/main/java";
         Schema schema = new Schema(1000, pkg + ".daogen");
 
-        addMyLibraryManga(schema);
+        addUserLibraryManga(schema);
         new DaoGenerator().generateAll(schema, src);
     }
 
-    private static void addMyLibraryManga(Schema schema) {
-        Entity note = schema.addEntity("MyLibraryMangaCard");
-        note.addIdProperty();
-        note.addStringProperty("title").notNull();
+    private static void addUserLibraryManga(Schema schema) {
+        Entity userLibraryManga = schema.addEntity("UserLibraryManga");
+        userLibraryManga.addIdProperty();
+        userLibraryManga.addStringProperty("tabs"); //for which tab/tabs
+        userLibraryManga.addStringProperty("title").notNull();
+        userLibraryManga.addStringProperty("imageURL");
+        userLibraryManga.addStringProperty("status");
+        userLibraryManga.addLongProperty("lastChapterDate");
+        userLibraryManga.addIntProperty("hits");
     }
 }
+
