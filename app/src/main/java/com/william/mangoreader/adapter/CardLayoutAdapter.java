cdb8dd9dba374e4b8db88aa4c7d3a82c9dcaa0cf
Clarence Mah <clarence.iyan@gmail.com>
2015-09-04 21:29:11 -0700

    implemented library categories; mangaEdenId is now primary key

--- app/src/main/java/com/william/mangoreader/adapter/CardLayoutAdapter.java
+++ app/src/main/java/com/william/mangoreader/adapter/CardLayoutAdapter.java
@@ -1,7 +1,10 @@
 package com.william.mangoreader.adapter;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -62,18 +65,39 @@ public class CardLayoutAdapter extends RecyclerView.Adapter<CardLayoutAdapter.Ca
         notifyDataSetChanged();
     }
 
-    private void addToLibrary(MangaEdenMangaListItem m) {
-        UserLibraryManga mangaItem = new UserLibraryManga(
-                null,
-                m.id,
-                activity.getString(R.string.library_page1),
-                m.title,
-                m.imageUrl,
-                m.status,
-                m.lastChapterDate,
-                m.hits);
-        ((MangoReaderActivity) activity).userLibraryMangaDao.insert(mangaItem);
-        Toast.makeText(activity, "\"" + m.title + "\" added to your library.", Toast.LENGTH_SHORT).show();
+    /**
+     * Creates dialog for user to select library category to add to.
+     *
+     * @param m
+     */
+    private void addToLibrary(final MangaEdenMangaListItem m) {
+        final UserLibraryManga[] mangaItem = new UserLibraryManga[1];
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Choose a category");
+        builder.setItems(R.array.library_categories, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mangaItem[0] = new UserLibraryManga(
+//                null, //TODO need more robust primary key
+                        m.id,
+                        activity.getResources().getStringArray(R.array.library_categories)[which],
+                        m.title,
+                        m.imageUrl,
+                        m.status,
+                        m.lastChapterDate,
+                        m.hits);
+
+                ((MangoReaderActivity) activity).userLibraryMangaDao.insert(mangaItem[0]);
+                String added = "\"" + m.title + "\" added to your library.";
+                Snackbar
+                        .make(activity.findViewById(R.id.drawer_layout), added, Snackbar.LENGTH_LONG)
+                                // .setAction() //TODO add undo on click
+                        .show();
+            }
+        });
+        builder.show();
+
+
     }
 
     private void removeFromLibrary(MangaEdenMangaListItem m) {
@@ -85,7 +109,11 @@ public class CardLayoutAdapter extends RecyclerView.Adapter<CardLayoutAdapter.Ca
         } else {
             UserLibraryManga mangaItem = (UserLibraryManga) l.get(0);
             ((MangoReaderActivity) activity).userLibraryMangaDao.delete(mangaItem);
-            Toast.makeText(activity, "\"" + m.title + "\" removed from your library.", Toast.LENGTH_SHORT).show();
+            String removed = "\"" + m.title + "\" removed from your library.";
+            Snackbar
+                    .make(activity.findViewById(R.id.drawer_layout), removed, Snackbar.LENGTH_LONG)
+                            // .setAction() //TODO add undo on click
+                    .show();
         }
     }
 
